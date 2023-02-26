738db8f6b87e20e75b20994cdf1e7d0fb6202884
建东 <37503208+ddshiyu@users.noreply.github.com>
2023-02-26 15:41:33 +0800

    boomb(delete): youpiayun.js

--- youpiayun.js
+++
@@ -1,7 +0,0 @@
-const footer = document.querySelector('.footer-wrapper')
-const firstSpan = footer.querySelector('span')
-const div = document.createElement('div')
-
-div.innerHTML = '<div class="1">本网站由<img src="https://cdn.jsdelivr.net/gh/ddshiyu/zredImage@master/youpai-logo.png">提供CDN加速/云服务存储</div>'
-
-footer.insertBefore(div, firstSpan)
