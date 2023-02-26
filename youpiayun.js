96daaa19f5e935866e7260771487160d693c0782
建东 <37503208+ddshiyu@users.noreply.github.com>
2023-02-26 15:41:37 +0800

    boomb(create): youpiayun.js

---
+++ youpiayun.js
@@ -0,0 +1,5 @@
+const youpeiyunfooter = document.querySelector('.footer-wrapper')
+const youpeiyunSpan = youpeiyunfooter.querySelector('span')
+const youpeiyundiv = document.createElement('div')
+youpeiyundiv.innerHTML = '<div style="margin-bottom: 10px;color: #999;">本网站由<img width="50" style="transform: translateY(6px);" src="https://cdn.jsdelivr.net/gh/ddshiyu/zredImage@master/youpai-logo.png">提供CDN加速/云服务存储</div>'
+youpeiyunfooter.insertBefore(youpeiyundiv, youpeiyunSpan)
